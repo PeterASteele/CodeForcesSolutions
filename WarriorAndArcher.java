c1e9f74f5862164534d85b9dba2d421c9678c158
PeterASteele <peter707@vt.edu>
2015-11-08 19:04:32 -0500

    Fixed problem naming

--- WarriorAndArcher.java
+++ WarriorAndArcher.java
@@ -1,53 +1,51 @@
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.function.Function;
+
 
 public class WarriorAndArcher {
-//http://codeforces.com/contest/595/problem/C
+//http://codeforces.com/contest/595/problem/D
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		int numberOfComp = input.nextInt();
-		int radius = input.nextInt();
-		int v = input.nextInt();
-		int[] compStart = new int[numberOfComp];
-		int[] compEnd = new int[numberOfComp];
-		for (int a = 0; a < numberOfComp; a++) {
-			compStart[a] = input.nextInt();
-			compEnd[a] = input.nextInt();
+		int startingPositions = input.nextInt();
+		int[] starting = new int[startingPositions];
+		for(int a = 0; a < startingPositions; a++){
+			starting[a] = input.nextInt();
 		}
-		for (int a = 0; a < numberOfComp; a++) {
-			double turnfrac = (compEnd[a] - compStart[a])
-					/ (2 * Math.PI * radius);
-			int totalTurns = (int) turnfrac;
-			double extraDistance = (turnfrac - totalTurns)
-					* (2 * Math.PI * radius);
-			double x0 = binarySearch((x) -> radius * x + 2 * Math.sin(x/2.0) * radius, 0, 2*Math.PI, extraDistance);
-			double totalTurnsAnswer = x0/(2*Math.PI) + totalTurns;
-			double fakeDistance = totalTurnsAnswer * 2 * Math.PI * radius;
-			double time = fakeDistance/v;
-			System.out.println(time);
+		Arrays.sort(starting);
+		int bans = (startingPositions-2)/2;
+		double change = Math.pow(2, 31);
+		double delta = change/2.0; //no more 1.9 rip
+		while(true){
+			int total = 0;
+			int nonban = starting[0];
+			int index = 1;
+			
+			while(total <= bans){
+				if(starting[index] - nonban >= change){
+					nonban = starting[index];
+				}
+				else{
+					total++;
+				}
+				index++;
+				if(index >= starting.length){
+					break;
+				}
+			}
+			if(total <= bans){
+				change += delta;
+				delta /= 2.0;
+			}
+			else{
+				change -= delta;
+				delta /= 2.0;
+			}
+			if(delta < 2 * Math.ulp(delta)){
+				break;
+			}
 		}
+		System.out.println((int) (change));
 	}
-
-	static double binarySearch(Function<Double, Double> f, double low,
-			double high, double y) {
-		while (Math.ulp(low) < (high - low)) {
-			double mid = (low + high) / 2.0;
-			double midVal = f.apply(mid);
-
-			if (midVal == y) // unlikely, but possible
-				return mid;
-			else if (midVal < y)
-				low = mid;
-			else if (midVal > y)
-				high = mid;
-		}
-		return (low + high) / 2.0;
-	}
-
-	/* Outputs 10.000000 */
-	
-	// System.out.printf("%.6f%n", x0);
 
 }
